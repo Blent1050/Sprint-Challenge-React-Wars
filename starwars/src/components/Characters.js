3c16af3ad67a797bcb79e635eb901dcc0737a1cf
Brandon Lent <blent1050@yahoo.com>
2019-01-11 11:56:17 -0500

    Component Characters created: handles rendering each character in the api - complete

---
+++ starwars/src/components/Characters.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './StarWars.css';
+
+function Characters(props){
+    console.log(props);
+    return(
+        <div className='characters-container'>
+        {
+            props.starwarsChars.map(char => {
+                return(
+                <div className='character'>
+                    <h2>{char.name}</h2>
+                    <p>Was born on {char.birth_year}</p>
+                </div>
+                ) 
+            })
+        }
+        </div>
+    )
+    
+}
+
+export default Characters;
